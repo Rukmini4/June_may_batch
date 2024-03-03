3f96e4944165c40730ecca4d025458e32149a338
Rukmini4 <https://github.com/Rukmini4/may_June_batch.git>
2024-03-03 19:36:25 +0530

    string methods

--- stringmethods.js
+++ stringmethods.js
@@ -26,7 +26,16 @@ let r = 'Pune method replaces  a specified pune value with another pune value in
 let q = r.replace(/pune/,'mumbai');
 console.log(q);
 
-let p = r.replaceAll(/pune/g,'mumbai');
+let j = r.replace(/pune/g,'mumbai');
+console.log(j);
+
+let i = r.replace(/pune/i,'mumbai');
+console.log(i);
+
+let k = r.replace(/pune/gi,'mumbai');
+console.log(k);
+
+let p = r.replaceAll('pune','mumbai')
 console.log(p);
 
 
@@ -38,3 +47,87 @@ console.log(f_name);
 
 let F_name = FN.concat('', LN,'', 'Bhapkar')
 console.log(F_name);
+
+//toLowerCase()
+let city = 'MumB KKKKKKKai DFFFFF cvfcvff';
+let city1 = city.toLowerCase();
+console.log('city >>>',city1);
+
+//toUpperCase()
+let city2 = city.toUpperCase();
+console.log('city >>>',city2);
+
+let name3 = '   Rukmini ';
+
+//trim
+let name4 = name3.trim();
+console.log('name4>>', name4);
+
+//trimStart()
+let name5 = name3.trimStart();
+console.log('name5',name5);
+
+//trimEnd()
+let name6 = name3.trimEnd();
+console.log('name6',name6);
+
+//charAt()
+let text5 = 'Rukmini Jagdale';
+console.log(text5.charAt(3));
+
+//split() always return array
+
+let dob = '24_06_1999';
+
+let newDob = dob.split('_');
+console.log(newDob);
+console.log(newDob[2]);
+
+let u = 'Jagdale A Patil';
+let u2 = u.split("");
+console.log(u2);
+console.log(u2[0]);
+console.log(u2[2]);
+
+// String Search Methods
+// String indexOf()
+let d ='dog hhhg gggg';
+console.log(d.indexOf('g'));//first match ind
+console.log('last ind',d.lastIndexOf('g')); //last match ind
+
+let text2 = "please locatefhfhfhfjf where 'locate' occurs!";
+console.log(text2.search("locate"))
+console.log(text2.search(/locate/))
+
+let text3 = "Please locatek jkjkjkkskks who asere 'locate occurs!";
+console.log(text3.match('ase'));
+console.log(text3.match(/ase/));
+console.log(text3.match(/ase/g));
+
+let text4 = "Hello world, welcome to the universe";
+console.log(text4.includes("world"));
+
+// let a1 = {
+//     "employees": [
+//         { "firstName": "John", "lastName": "Doe" },
+//         { "firstName": "Anna", "lastName": "Smith" },
+//         { "firstName": "Peter", "lastName": "Jones" }
+//     ],
+//     statusCode:2001,
+//     successMsg:'Data saved succussfully'
+// }
+
+//startswitch()
+let msg = 'hhh 200 Success';
+console.log(msg.startsWith('20'));
+
+text7 = "john Doe"
+console.log(text7.endsWith('e'));
+
+text7 = "Jo Doe h Doen Doe";
+console.log(Array.from(text7.matchAll('Doe')));
+
+
+
+
+
